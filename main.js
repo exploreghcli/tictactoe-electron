5d178754e672d505ae070c74da9d6e731616957b
exploreghcli <exploreghcli@example.com>
2026-06-13 15:23:35 -0700

    Increase window size and allow resizing so UI buttons fit

--- main.js
+++ main.js
@@ -3,9 +3,12 @@ const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 420,
-    height: 520,
-    resizable: false,
+    width: 640,
+    height: 800,
+    minWidth: 480,
+    minHeight: 640,
+    resizable: true,
+    center: true,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
